721b8604a25e160110ce8579d4f70b36cf067075
UP240248 <up240248@alumnos.upa.edu.mx>
2025-06-11 19:50:40 -0600

    trabajos

---
+++ dia2.py
@@ -0,0 +1,31 @@
+#day 2 excercise 
+first=1
+last=2
+nombres="diego abiud"
+apellidos="figueroa carlos"
+nombre_completo="diego abiud figueroa carlos"
+pais="mexico"
+estado="aguascalientes"
+edad=19
+esta_casado="falso "
+it_light_on="verdadero"
+
+
+#excercise 2
+type(first=1)
+type(last=2)
+len(nombres)
+len(apellidos)
+num_one=5
+num_two=4
+total=num_one+num_two
+subtraccion=num_one-num_two
+diff=num_two - num_one
+multiplication= num_two*num_one
+division=num_two/num_one
+remainder=num_two%num_one
+exp= num_two**num_one
+floor_division=num_two//num_one
+radius_of_circle=30
+pi=3.1416
+area_of_circle=pi*radius_of_circle/2
---
+++ dia3.py
@@ -0,0 +1,7 @@
+Nombres=["diego","abiud","carlos","figueroa"]
+edades="20","16","19","18"
+frutas={"manzana","pera","naranja"}
+estatura=1.69
+estudiandte="verdadero"
+print(len(Nombres))
+print(type(Nombres))
